6014992d994183ebda02e1a0168e6e64b8c6f717
Edgar Costa <edgarcosta@math.dartmouth.edu>
2017-10-01 13:46:46 -0600

    Update controlled_reduction.pyx

--- pycontrolled_reduction/controlled_reduction.pyx
+++ pycontrolled_reduction/controlled_reduction.pyx
@@ -1,6 +1,6 @@
 # Copyright 2017 Jennifer Balakrishnan, Edgar Costa 
 # See LICENSE file for license details.
-# distutils: language = c++
+# distutils: language = C++
 
 from cysignals.signals cimport sig_on, sig_off
 from sage.rings.integer_ring import ZZ
@@ -8,16 +8,16 @@ from sage.rings.polynomial.polynomial_ring import PolynomialRing
 from sage.libs.ntl.ntl_ZZX import ntl_ZZX
 
 def controlled_reduction(f, p, verbose = False):
-    cdef vector[int64_t] coef;
-    cdef vector[ vector[int64_t] ] keys;
+    cdef vector[int64_t] coef
+    cdef vector[ vector[int64_t] ] keys
     for mvec, c in f.dict().iteritems():
         coef.push_back(c % p)
         keys.push_back(mvec)
-    cdef ntl_ZZX zeta = ntl_ZZX();
-    zeta_function(zeta.x, keys, coef, p);
+    cdef ntl_ZZX zeta = ntl_ZZX()
+    zeta_function(zeta.x, keys, coef, p)
     # convert zeta to a sage polynomial
     poly=[]
     for i in range(zeta.degree()+1):
         poly.append(zeta[i]._integer_())
 
-    return PolynomialRing(ZZ, 'T')(poly).reverse();
+    return PolynomialRing(ZZ, 'T')(poly).reverse()
