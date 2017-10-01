872997eb61f989d326c4742853339f2f4680ad0d
Edgar Costa <edgarcosta@math.dartmouth.edu>
2017-10-01 13:50:39 -0600

    Update controlled_reduction.pyx

--- pycontrolled_reduction/controlled_reduction.pyx
+++ pycontrolled_reduction/controlled_reduction.pyx
@@ -5,7 +5,7 @@
 from cysignals.signals cimport sig_on, sig_off
 from sage.rings.integer_ring import ZZ
 from sage.rings.polynomial.polynomial_ring import PolynomialRing
-from sage.libs.ntl.ntl_ZZX import ntl_ZZX
+from sage.libs.ntl.ntl_ZZX cimport ntl_ZZX
 
 def controlled_reduction(f, p, verbose = False):
     cdef vector[int64_t] coef
